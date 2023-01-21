4b29371e4d50fcf711fddbca2d0f2020a25e751b
Alex Naga <alex.naga@hotmail.com>
2023-01-21 07:19:20 +0100

    fix: add support for os new lines, aws default region

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,7 @@ export declare global {
   namespace NodeJS {
     interface ProcessEnv {
       AWS_URL: string;
+      AWS_DEFAULT_REGION: string;
       USER_EMAIL: string;
       USER_PASSWORD: string;
       DEBUG?: string;
